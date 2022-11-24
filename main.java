8d2b6a45ff93a28b6eb81c7d1476d375da674bd4
sanifaaaa <115127468+sanifaaaa@users.noreply.github.com>
2022-11-24 10:56:48 +0700

    Add files via upload

---
+++ main.java
@@ -0,0 +1,18 @@
+
+/*
+Online Java - IDE, Code Editor, Compiler
+
+Online Java is a quick and easy tool that helps you to build, compile, test your programs online.
+*/
+
+public class Main
+{
+    public static void main(String[] args) {
+        String nama = "Sanifa lihawa";
+        int umur = 2004;
+        
+        System.out.println("Assalamualaikum");
+        System.out.println("Nama saya :"+ nama);
+        System.out.println("Saya lahir ditahun :"+ umur);
+    }
+}
